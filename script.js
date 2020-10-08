deb0d0f572cb3450651256781d46eedfb7549fb7
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2020-10-08 20:40:08 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,15 @@
+let money = 20;
+let income = 25;
+let addExpenses = 30;
+const deposit = 35;
+const mission = 40;
+const period = 45;
+
+
+
+console.log(money);
+
+alert('Hello world!');
+
+console.log('Hello friends!');
+
