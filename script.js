b677d5e50c2b7aaf5a7c9417c512063a95f2428c
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2020-10-09 19:57:28 +0300

    Добавил переменные, воспользовался разными методами

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let money = 20;
-let income = 25;
-let addExpenses = 30;
-const deposit = 35;
-const mission = 40;
-const period = 45;
+let money = 30000;
+let income = 'Фриланс';
+let addExpenses = 'Интернет, Такси, Комунналка';
+const deposit = true;
+const mission = 1000000000000000;
+const period = 6;
+let budgetDay 
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(','));
+console.log(money / 30);
+
+
+    
 
 
 
-console.log(money);
 
-alert('Hello world!');
 
-console.log('Hello friends!');
 
