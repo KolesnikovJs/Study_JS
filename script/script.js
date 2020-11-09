9f65844f096fab14c562bc72aaa5a8b42ebe5dab
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2020-11-09 21:18:25 +0300

    add lesson18

---
+++ script/script.js
@@ -0,0 +1,45 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+ //Timer
+ 
+ function countTimer(deadline) {
+    let timerHours = document.querySelector('#timer-hours'),
+        timerMinutes = document.querySelector('#timer-minutes'),
+        timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaining() {
+        let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+        return {
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+            'timeRemaining': timeRemaining
+        };
+    }
+
+    function updateClock() {
+        let timer = getTimeRemaining();
+    
+
+        timerHours.textContent = timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        timerSeconds.textContent = timer.seconds;
+        if(timer.timeRemaining > 0) {
+            setTimeout(updateClock, 1000);
+        }
+    }
+
+    updateClock();
+     
+     
+ }
+
+    countTimer('15 november 2020');
+
+});
