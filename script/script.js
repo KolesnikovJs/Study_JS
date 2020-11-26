aec6f0cbc70df3da4ab45efc59d110a2af1842ac
Vitalik Kolesnikov <webkolesnikov77@gmail.com>
2020-11-26 22:17:21 +0300

    add lesson25

--- script/script.js
+++ script/script.js
@@ -143,11 +143,24 @@ const calc = (price = 100) => {
 
     const countSum = () => {
         let total = 0;
+        let countValue = 1;
+        let dayValue = 1;
         const typeValue = calcType.options[calcType.selectedIndex].value;
         const squareValue = +calcSquare.value;
 
+        if (calcDay.value && calcDay.value < 5) {
+            dayValue *= 2;
+        } else if (calcDay.value && calcDay.value < 10) {
+            dayValue *= 1.5;
+        }
+
+
+        if (calcCount.value > 1) {
+            countValue += (calcCount.value - 1) / 10;
+        }
+
         if (typeValue && squareValue) {
-            total = price * typeValue * squareValue;
+            total = price * typeValue * squareValue * countValue * dayValue;
         } else {
             total = 0;
         }
